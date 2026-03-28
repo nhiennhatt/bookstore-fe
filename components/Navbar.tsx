b4864323719e6f2eb6d3879bdc1a9b72fc708e42
nhiennhatt <hiennhatt1804@gmail.com>
2026-03-28 21:24:26 +0700

    feat: initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+  return (
+    <div className="border-b-2 border-gray-200 flex justify-between items-center p-4">
+      <div className="py=2">
+        <img src="/logo.svg" alt="logo" className="w-16" />
+      </div>
+    </div>
+  );
+}
